cb784ecbdaf0278a12a934bbe2e88bbb69aac5bb
lipeiv <lipeiv@qq.com>
2021-09-27 21:25:24 +0800

    gitignore

--- helloNode/test.js
+++ helloNode/test.js
@@ -5,7 +5,7 @@ var server = http.createServer()
 server.on('request', function(request, response) {
 	console.log("请求路径:" + request.url)
 
-	response.write("Hello <nbs>")
+	response.write("Hello <nbs>"+request.toString())
 	response.end("end")
 })
 
