458e67df550e50a82a0f94f0e6d293d5e769ec25
lipeiv <lipeiv@qq.com>
2021-11-19 23:26:52 +0800

    log4js

---
+++ log4/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const log4js = require("log4js");
+
+log4js.configure({
+    appenders: {test: {type: "file",filename: "test.log"}},
+    categories: {default: {appenders: ["test"], level: "info"}}
+});
+
+const logger = log4js.getLogger();
+
+
+//fs.writeFileSync("test.log", "utf-8");
+
+fs.readFile("test.log", "utf-8", (err, data) =>{
+    console.log("err>>", err);
+    console.log(data);
+})
+
+
+logger.info("time:", new Date());
+
